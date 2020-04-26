99e7791881a409db4601c070c787519a328703a9
yurevsal <41169404+yurevsal@users.noreply.github.com>
2020-04-26 13:23:53 -0300

    Update display.py

--- display.py
+++ display.py
@@ -4,4 +4,8 @@ from uiflow import *
 
 setScreenColor(0x222222) #set backgrondColor RRGGBB
 
-lcd.qrcode('Hello world', 72, 32, 176) #lcd.qrcode(message, x, y, size)
+#lcd.qrcode('Hello world', 72, 32, 176) #lcd.qrcode(message, x, y, size)
+
+title0 = M5Title(title="Title_001", x=12 , fgcolor=0x00d5ff, bgcolor=0x4f4f4f)#titleBar
+
+image = M5Img(111, 56, "res/default.jpg", True) #M5Img(x, y, imgPath, visible)
